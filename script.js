6c71ae09471de305c45e4bd725baf82b0709b8b4
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-16 17:19:45 +0300

    feat(script.js) выполнить доп. ДЗ lesson14

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ class DomElement {
     this.bg = bg;
     this.fontSize = fontSize;
   }
-  createElement(text) {
+  createElement(text, position) {
     let element;
     this.text = text;
-    console.log(this.selector);
+    this.position = position;
     if (this.selector.startsWith('.')) {
       element = document.createElement('div');
       element.classList.add(this.selector.slice(1));
@@ -22,18 +22,32 @@ class DomElement {
     element.style.cssText = `height: ${this.height};
       width: ${this.width};
       background: ${this.bg};
-      font-size: ${this.fontSize};               
+      font-size: ${this.fontSize};
+      position: ${this.position};               
     `;
     element.textContent = this.text;
     return document.body.append(element);
   }
-  
 }
 
-let block = new DomElement('.block', '100px', '100px', 'lightblue', '30px');
+let block = new DomElement('.block', '100px', '100px', 'purple', '30px');
+document.addEventListener("DOMContentLoaded", () => {
+  block.createElement('', 'absolute');
+});
 
-block.createElement('Hello');
-
-let p = new DomElement('#block', '300px', '400px', 'lightgreen', '50px');
-
-p.createElement('Bye');
+window.addEventListener('keydown', function(e) {
+  let square = document.querySelector('.block');
+  let computedStyle = getComputedStyle(square);
+  function callback(step, position) {
+    return (step + parseInt(position) + 'px');
+  }
+ if (e.code === 'ArrowUp') {
+  square.style.bottom = callback(10, computedStyle.bottom);
+ } else if (e.code === 'ArrowDown') {
+  square.style.bottom = callback(-10, computedStyle.bottom);
+ } else if (e.code === 'ArrowRight') {
+  square.style.right = callback(10, computedStyle.right);
+ } else if (e.code === 'ArrowLeft') {
+  square.style.right = callback(-10, computedStyle.right);
+ } 
+});
